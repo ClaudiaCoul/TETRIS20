c3c5ab7921b8cc91dc016cde2cb2dd13f3eec7b4
austinFho <39381490+austinFho@users.noreply.github.com>
2018-05-18 09:19:42 -0700

    Create BricksPanel.java

---
+++ BricksPanel.java
@@ -0,0 +1,27 @@
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class BricksPanel extends JPanel {
+
+  private static final long serialVersionUID = 1L;
+  private static final int ROWS = 15;
+  private static final int COLS = 10;
+  private static final int WIDTH = COLS * TILE_SIZE;
+  private static final int HEIGHT = ROWS * TILE_SIZE;
+  
+  public Dimension getPreferredSize(){
+    Dimension size = new Dimension(WIDTH, HEIGHT);
+    return size;
+  }
+  
+  public void paintComponent(Graphics g) {
+    //Background
+    g.setColor(Color.BLACK);
+    g.fillRect(0, 0, WIDTH, HEIGHT);
+    
+    // fallen bricks
+    
+    // falling bricks
+  }
+
+}
