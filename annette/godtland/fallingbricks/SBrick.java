54f88f5f472381e143852d71a60f12d8f584a326
Amanda <39381413+amandaj18@users.noreply.github.com>
2018-05-21 09:17:14 -0700

    Update SBrick.java

--- annette/godtland/fallingbricks/SBrick.java
+++ annette/godtland/fallingbricks/SBrick.java
@@ -18,4 +18,19 @@ public class SBrick extends Brick {
       image = FileIO.readImageFile(this);
     }
   }
+  
+  public void draw(Graphics g) {
+    int rows = TILES.length;
+    int cols = TILES[0].length;
+    
+    for (int row = 0; row <= rows; row++) {
+      for (int col = 0; col <= cols; col++) {
+        if (TILES[row][col] == true) {
+          int tileX = x + (TILE_SIZE * col);
+          int tileY = y + (TILE_SIZE * row);
+          g.drawImage(image, tileX, tileY, null);
+        }
+      }//end col forloop
+    }//end row forloop
+  }//end draw method
 }
