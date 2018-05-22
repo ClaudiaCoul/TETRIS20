6d1b0b1b4c548e15bad2b235bff0b866b8342808
austinFho <39381490+austinFho@users.noreply.github.com>
2018-05-22 09:23:15 -0700

    Update OBrick.java

--- annette/godtland/fallingbricks/OBrick.java
+++ annette/godtland/fallingbricks/OBrick.java
@@ -1,5 +1,54 @@
 package annette.godtland.fallingbricks;
 
-public class OBrick extends Brick{
+public class OBrick extends Brick{  
+  private static final string BRICK_FILE "/yellowBrick.jpg";
+  private static final boolean[][] TILES = {{true, true},
+                                            {true, true}};
+  private static BufferedImage image;
 
+  public OBrick(int row, int col) {
+    super(row, col);
+    if (image == null) {
+      image = FileIO.readImageFile(this, BRICK_FILE);
+    }
+  }
+  
+  public void rotateLeft() {
+    
+  }
+  
+  public void rotateRight() {
+    
+  }
+  
+  public void draw(Graphics g) {
+    int rows = TILES.length;
+    int cols = TILES[0].length;
+    
+    for (int row = 0; row <= rows; row++) {
+      for (int col = 0; col <= cols; col++) {
+        if (TILES[row][col] == true) {
+          int tileX = x + (TILE_SIZE * col);
+          int tileY = y + (TILE_SIZE * row);
+          g.drawImage(image, tileX, tileY, null);
+        }
+      }//end col forloop
+    }//end row forloop
+  }//end draw method
+  
+  public boolean hasTileAt(int row, int col){
+    return TILES[row][col];
+  }
+  
+  public int getNumberOfRows(){
+    return TILES.length;
+  }
+  
+  public int getNumberOfColumns(){
+    return TILES[0].length;
+  }
+  
+  public BufferedImage getTileImage(){
+    return image;
+  }
 }
