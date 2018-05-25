86a10ff8b408e6169ee158f8d9539a4ff62ef8f2
Amanda <39381413+amandaj18@users.noreply.github.com>
2018-05-25 09:40:39 -0700

    Update MyTimer.java

--- annette/godtland/mytimer/MyTimer.java
+++ annette/godtland/mytimer/MyTimer.java
@@ -6,6 +6,32 @@ public class MyTimer extends JFrame {
   private static final long serialVersion UID = 1L;
   
   public static void main(String[] args) {
+    try {
+      String className = UIManager.getCrossPlatformLookAndFeelClassName();
+      UIManager.setLookAndFeel(className);
+    }
+    catch (Exception e) {}
+    
+    SwingUtilities.invokeLater(new Runnable() {
+      public void run() {
+        new MyTimer();
+      }
+    });
+  }
   
+  public MyTimer() {
+    initGUI();
+    
+    setTitle("My Timer");
+    setResizable(false);
+    pack();
+    setLocationRelativeTo(null);
+    setVisable(true);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+  }
+  
+  public void initGUI() {
+    TitleLabel titleLabel = new TitleLabel("My Timer");
+    add.(titleLabel, BorderLayout, PAGE_START);
   }
 }
