c6c2d8e2437d0cb0ffb140263497c3c240028498
Amanda <39381413+amandaj18@users.noreply.github.com>
2018-05-25 09:32:11 -0700

    Create MyTimer.java

---
+++ annette/godtland/mytimer/MyTimer.java
@@ -0,0 +1,11 @@
+package annette.godtland.mytimer;
+
+import javax.swing.JFrame;
+
+public class MyTimer extends JFrame {
+  private static final long serialVersion UID = 1L;
+  
+  public static void main(String[] args) {
+  
+  }
+}
