4d097ca7c88928545b0518cfc794beb5c3a5c1fe
Amanda <39381413+amandaj18@users.noreply.github.com>
2018-05-25 09:44:16 -0700

    Update TimerPanel.java

--- annette/godtland/mytimer/TimerPanel.java
+++ annette/godtland/mytimer/TimerPanel.java
@@ -8,4 +8,15 @@ public class TimerPanel extends JPanel {
   private int width = 150;
   private int height = 24;
   private String timeString = "00:00:00";
+  
+  public void paintComponent (Graphics g) {
+    super.paintComponent(g);
+    g.setColor(Color.BLACK);
+    g.drawString(timeString, 0, height);
+  }
+  
+  public Dimension getPreferredSize() {
+    Dimension size = new Dimension(width, height);
+    return size;
+  }
 }
