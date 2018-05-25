c681182cbc663656b19e4349211798025d6d7c24
Amanda <39381413+amandaj18@users.noreply.github.com>
2018-05-25 09:42:33 -0700

    Create TimerPanel.java

---
+++ annette/godtland/mytimer/TimerPanel.java
@@ -0,0 +1,11 @@
+package annette.godtland.mytimer;
+
+import javax.swing.JPanel;
+
+public class TimerPanel extends JPanel {
+  private static final long serialVersionUID = 1L;
+  
+  private int width = 150;
+  private int height = 24;
+  private String timeString = "00:00:00";
+}
