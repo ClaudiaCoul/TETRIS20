04c3c405cf59cdfd48050acebf2e231304c32ac7
Amanda <39381413+amandaj18@users.noreply.github.com>
2018-05-31 09:22:46 -0700

    Update MyTimer.java

--- annette/godtland/mytimer/MyTimer.java
+++ annette/godtland/mytimer/MyTimer.java
@@ -5,7 +5,8 @@ import javax.swing.JFrame;
 public class MyTimer extends JFrame {
   private static final long serialVersionUID = 1L;
   
-  private TimerPanel timerPanel = new TimerPanel();
+  private Font font = new Font(Font.DIALOG, Font.BOLD, 36);
+  private TimerPanel timerPanel = new TimerPanel(10, font);
   
   public static void main(String[] args) {
     try {
