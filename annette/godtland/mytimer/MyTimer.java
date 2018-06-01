d238b769d721291a97a47f683c0790030ceeb686
Amanda <39381413+amandaj18@users.noreply.github.com>
2018-06-01 09:45:30 -0700

    Update MyTimer.java

--- annette/godtland/mytimer/MyTimer.java
+++ annette/godtland/mytimer/MyTimer.java
@@ -39,16 +39,35 @@ public class MyTimer extends JFrame {
   private void stop() {
     timePanel.stop();
   }
+  private void addAnHour() {
+    long time = timerPanel.getTime();
+    time += 3600;
+    timerPanel.setTime(time);
+  }
+  
+  private void addAMinute() {
+    long time = timerPanel.getTime();
+    time += 60;
+    timerPanel.setTime(time);
+  }
+  private void clear() {
+    timerPanel.stop();
+    timerPanel.setTime(0);
+  }
   
   public void initGUI() {
     TitleLabel titleLabel = new TitleLabel("My Timer");
     add.(titleLabel, BorderLayout, PAGE_START);
     
     JPanel centerPanel = new JPanel();
+    centerPanel.setBackground(Color.MAGENTA);
     add(centerPanel, BorderLayout.CENTER);
     
+    timerPanel.setBackground(Color.MAGENTA);
     centerPanel.add(timerPanel);
+    
     JPanel buttonPanel = new JPanel();
+    buttonPanel.setBackground(Color.BLACK);
     add(buttonPanel, BorderLayout.PAGE_END);
     
     JButton hoursButton = new JButton("Hour");
@@ -66,6 +85,14 @@ public class MyTimer extends JFrame {
       }
     });
     buttonPanel.add(minutesButton);
+    
+    JButton clearButton = new JButton("Clear");
+    clearButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        clear();
+      }
+    });
+    buttonPanel.add(clearButton);
     
     JButton startButton = new JButton("Start");
     startButton.addActionListener(new ActionListener() {
