5805c0f9cf88fac71dbecdf91f813a0d36144da7
austinFho <39381490+austinFho@users.noreply.github.com>
2018-06-01 09:44:14 -0700

    Update SBrick.java

--- annette/godtland/fallingbricks/SBrick.java
+++ annette/godtland/fallingbricks/SBrick.java
@@ -7,7 +7,7 @@ import java.awt.image.BufferedImage;
 
 public class SBrick extends Brick {
   
-  private static final string BRICK_FILE = "/redBrick.jpg";
+  private static final String BRICK_FILE = "/redBrick.jpg";
   private static final boolean[][][]TILES = {{{false, true, true},
                                              {true, true, false}},
                                              
