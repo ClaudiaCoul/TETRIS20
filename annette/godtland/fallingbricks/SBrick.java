a1729f233f1e3c4a696304a8a15f15445bc69bb3
austinFho <39381490+austinFho@users.noreply.github.com>
2018-06-01 09:03:00 -0700

    Update SBrick.java

--- annette/godtland/fallingbricks/SBrick.java
+++ annette/godtland/fallingbricks/SBrick.java
@@ -7,7 +7,7 @@ import java.awt.image.BufferedImage;
 
 public class SBrick extends Brick {
   
-  private static final string BRICK_FILE "/redBrick.jpg";
+  private static final string BRICK_FILE = "/redBrick.jpg";
   private static final boolean[][][]TILES = {{{false, true, true},
                                              {true, true, false}},
                                              
@@ -15,7 +15,7 @@ public class SBrick extends Brick {
                                               {true, true},
                                               {true, false}}};
   private static BufferedImage image;
-  int state = 0;
+  private int state = 0;
   
   public SBrick(int row, int col) {
     super(row, col);
@@ -37,21 +37,6 @@ public class SBrick extends Brick {
       state = 0;
     }
   }
-  
-  public void draw(Graphics g) {
-    int rows = TILES.length;
-    int cols = TILES[0].length;
-    
-    for (int row = 0; row <= rows; row++) {
-      for (int col = 0; col <= cols; col++) {
-        if (TILES[row][col] == true) {
-          int tileX = x + (TILE_SIZE * col);
-          int tileY = y + (TILE_SIZE * row);
-          g.drawImage(image, tileX, tileY, null);
-        }
-      }//end col forloop
-    }//end row forloop
-  }//end draw method
   
   public boolean hasTileAt(int row, int col){
     return TILES[row][col];
