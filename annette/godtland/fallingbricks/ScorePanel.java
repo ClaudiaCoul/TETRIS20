c6a0d4c19d3a539729407f6f2cd09d1d6b78c196
ClaudiaCoul <39381394+ClaudiaCoul@users.noreply.github.com>
2018-06-04 09:12:12 -0700

    Update ScorePanel.java

--- annette/godtland/fallingbricks/ScorePanel.java
+++ annette/godtland/fallingbricks/ScorePanel.java
@@ -2,13 +2,35 @@ package annette.godtland.fallingBricks;
 import javax.swing.JPanel;
 public class ScorePanel extends JPanel {
   private static final long serialVersionUID = 1L;
+  private static final Font SMALL_FONT = new Font(Font.DIALOG, Font.PLAIN, 12);
+  private static final Font BIG_FONT = new Font(Font.DIALOG, Font.BOLD, 36);
+  
   private int initialScore = 0;
   private int score = 0;
+  private JLabel scoreLabel = new JLabel("0");
   
   public ScorePanel(int initialScore, Color color){
     this.initialScore = initialScore;
     score = initialScore;
     setBackground(color);
+    
+    JLabel scoreTitleLabel = new JLabel("Score: ");
+    scoreTitleLabel.setFont(SMALL_FONT);
+    add(scoreTitleLabel);
+    
+    scoreLabel.setFont(BIG_FONT);
+    add(scoreLabel);
   }
   
+  public void addToScore(int points){
+    score+=points;
+    scoreLabel.setText("" + score);
+  }
+  
+  public void reset() {
+    score = initialScore;
+    scoreLabel.setText("" + score);
+  }
+  
+}  
   
